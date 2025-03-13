e97137ebd9fba619d2149c79835138837199aaff
Rajesh Gajengi <119670590+Rajesh9825@users.noreply.github.com>
2025-03-14 01:16:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def predict_churn():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True,port=8080)
+    app.run(host="0.0.0.0",port=8080)
 
 
 
